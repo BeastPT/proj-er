99d1f8bf6f8190bb83d2e5fb2db8a49dbc158b81
BeastPT <ajfa2004@gmail.com>
2024-12-13 18:20:40 +0000

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,4 @@
-import jwt from 'jsonwebtoken'
-const SECRET_KEY = 'SECRET'
 import { getUserByData } from '../models/user.js'
-
 
 export default async function auth(req, res, next) { // Middleware de autenticação do utilizador
     try {
@@ -10,9 +7,8 @@ export default async function auth(req, res, next) { // Middleware de autentica�
             return res.status(401).json({message: 'Unauthorized User'})
         }
 
-        const user = jwt.verify(token, SECRET_KEY) // Verifica o token com a chave secreta
-        req.username = user.username // Adiciona o nome de utilizador ao pedido
-        req.user = await getUserByData({ username: user.username }) // Adiciona o utilizador ao pedido
+        req.email = user.email // Adiciona o nome de utilizador ao pedido
+        req.user = await getUserByData({ email: user.email }) // Adiciona o utilizador ao pedido
         next()
     } catch (err) {
         res.status(401).json({message: 'Unauthorized User'}) // Se o token for inválido, retorna 401
