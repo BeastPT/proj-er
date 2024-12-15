f5d0e4b93992d57febf6599b352f9515918e1a1d
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2024-12-15 13:37:20 +0000

    Revert "Update"
    This reverts commit c1b2f76bb03e2413a6d64247b52ab4e92e43865d.

--- frontend/src/components/HomePageMedico/HomePageMedico.tsx
+++ frontend/src/components/HomePageMedico/HomePageMedico.tsx
@@ -1,26 +1,17 @@
 import { Button, Paper, Stack, Title, Text } from '@mantine/core';
-import { useNavigate } from 'react-router-dom'; // Importa o hook useNavigate
 
 export function HomePageMedico() {
-    const navigate = useNavigate(); // Inicializa o hook de navegação
-
     return (
-        <Paper p="xl">
-            <Stack gap="xl" justify="flex-start">
+        <Paper p='xl'>
+            <Stack gap='xl' justify='flex-start'>
                 <div>
                     <Title order={2}>Mario Ferreira Cardoso</Title>
-                    <Text size="xs">Especialidade: Cardiologia</Text>
+                    <Text size='xs'>Especialidade:</Text> 
                 </div>
 
-                <Button variant="filled" onClick={() => navigate('/InserirDisponibilidade')}>
-                    Inserir Disponibilidade
-                </Button>
-                <Button variant="filled" onClick={() => navigate('/consulta/historico')}>
-                    Consultar Histórico Pacientes
-                </Button>
-                <Button variant="filled" onClick={() => navigate('planotratamento')}>
-                    Enviar Plano de Tratamento
-                </Button>
+                <Button variant="filled">Inserir Disponibilidade</Button>
+                <Button variant="filled">Consultar Histórico Pacientes</Button>
+                <Button variant="filled">Enviar Plano Tratamento</Button>
             </Stack>
         </Paper>
     );
