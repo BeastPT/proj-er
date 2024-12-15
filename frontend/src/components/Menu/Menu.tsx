c1b2f76bb03e2413a6d64247b52ab4e92e43865d
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2024-12-15 13:34:23 +0000

    Update
    Navegacao entre paginas

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,6 +1,9 @@
 import { Button, Paper, Stack, Title, Text } from '@mantine/core';
+import { useNavigate } from 'react-router-dom'; // Importa o hook useNavigate
 
 export function Menu() {
+    const navigate = useNavigate(); // Inicializa o hook de navegação
+
     return (
         <Paper p='xl'>
             <Stack gap='xl' justify='flex-start'>
@@ -8,12 +11,23 @@ export function Menu() {
                     <Title order={2}>Mario Ferreira Cardoso</Title>
                     <Text size='xs'>Paciente desde 02/12/2024</Text>
                 </div>
+                <Button variant="filled" onClick={() => navigate('/consulta/marcar')}>
+                    Marcar Consulta
+                </Button> 
 
-                <Button variant="filled">Marcar Consulta</Button>
-                <Button variant="filled">Cancelar Consulta</Button>
-                <Button variant="filled">Ver Exames</Button>
-                <Button variant="filled">Notificações</Button>
+                <Button variant="filled" onClick={() => navigate('/consulta/cancelar')}> 
+                    Cancelar Consulta
+                </Button>
+
+                <Button variant="filled" onClick={() => navigate('/consulta/exames')}>
+                    Ver Exames
+                </Button>
+
+                <Button variant="filled" onClick={() => navigate('/consulta/notificacoes')}>
+                    Notificações
+                </Button>
             </Stack>
         </Paper>
+        //falta criar as paginas
     );
 }
