1c3294ac86a06492b9145e1da76550fe04adcbe7
BeastPT <ajfa2004@gmail.com>
2024-12-16 12:17:21 +0000

    update

--- frontend/src/components/Consulta/ConsultaPaper/ConsultaPaper.tsx
+++ frontend/src/components/Consulta/ConsultaPaper/ConsultaPaper.tsx
@@ -1,20 +1,55 @@
-import { Paper, Title, Text } from '@mantine/core';
+import { Paper, Title, Text, useMantineColorScheme, Button } from '@mantine/core';
 import dayjs from 'dayjs';
+
+interface ConsultaPaperButtonProps {
+    onClick: () => void;
+    text: string;
+    style?: React.CSSProperties;
+    variant?: 'default' | 'light' | 'filled';
+    color?: string;
+}
 
 interface ConsultaPaperProps {
     status: 'concluida' | 'por realizar' | 'cancelada';
     especialidade: string;
     medico: string;
     timestamp: number;
+    button?: ConsultaPaperButtonProps;
 }
 
-export function ConsultaPaper({status, especialidade, medico, timestamp}: ConsultaPaperProps) {
+export function ConsultaPaper({ status, especialidade, medico, timestamp, button }: ConsultaPaperProps) {
+    const { colorScheme } = useMantineColorScheme();
     return (
-        <Paper shadow='sm' radius='md' p='md' style={{ backgroundColor: 'var(--mantine-color-dark-9)' }}>
-            <Title order={3}>Consulta {status}</Title>
-            <Text size='sm'>{especialidade}</Text>
-            <Text size='sm' mt='md'><strong>Médico</strong>: {medico}</Text>
-            <Text size='sm'><strong>Data da Consulta</strong>: {dayjs(timestamp).format("DD-MM-YYYY  HH:mm")}</Text>
+        <Paper
+            shadow="sm"
+            radius="md"
+            p="md"
+            style={{
+                backgroundColor: colorScheme === 'dark'
+                    ? 'var(--mantine-color-dark-9)'
+                    : 'var(--mantine-color-gray-2)',
+            }}
+        >
+                <div>
+                    <Title order={3}>Consulta {status}</Title>
+                    <Text size="sm">{especialidade}</Text>
+                    <Text size="sm" mt="md">
+                        <strong>Médico</strong>: {medico}
+                    </Text>
+                    <Text size="sm">
+                        <strong>Data da Consulta</strong>: {dayjs(timestamp).format('DD-MM-YYYY  HH:mm')}
+                    </Text>
+                </div>
+                {button && (
+                    <Button
+                        onClick={button.onClick}
+                        style={button.style}
+                        variant="light"
+                        mt={16}
+                    >
+                        {button.text}
+                    </Button>
+                )}
         </Paper>
     );
 }
--- frontend/src/components/Consulta/VerHistoricoConsulta/VerHistoricoConsulta.tsx
+++ frontend/src/components/Consulta/VerHistoricoConsulta/VerHistoricoConsulta.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack, Title, Text } from '@mantine/core';
+import { Paper, Stack, Title } from '@mantine/core';
 import { ConsultaPaper } from '../ConsultaPaper/ConsultaPaper';
 
 export function VerHistoricoConsulta() {
@@ -7,7 +7,8 @@ export function VerHistoricoConsulta() {
             <Title ta="center" order={1}>Histórico de Consultas</Title>
 
             <Stack gap='md' mt='md' justify='flex-start'>
-                <ConsultaPaper status='por realizar' especialidade='Pediatria' medico='Pedro da Mata' timestamp={new Date().getTime()}/>
+                <ConsultaPaper status='por realizar' especialidade='Pediatria' medico='Pedro da Mata' timestamp={new Date().getTime()}
+                button={{ onClick: () => console.log('oi'), text: 'Marcar Consulta' }} />
             </Stack>
         </Paper>
     );
